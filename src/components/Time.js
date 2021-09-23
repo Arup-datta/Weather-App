d40e1e2d209ae49260d3fd8e38a69153273cf750
Arup Datta <arup.cse.sust@gmail.com>
2021-09-23 13:23:29 +0600

    Time interval issue fixed

--- src/components/Time.js
+++ src/components/Time.js
@@ -2,7 +2,8 @@ import React, {useState, useEffect} from 'react'
 
 export const Time = () => {
 
-    const[state, changeState] = useState('');
+    const[state, setState] = useState('');
+    const [firstCall, setFirstCall] = useState(false);
 
     const getTime = () => {
         const d = new Date();
@@ -13,13 +14,22 @@ export const Time = () => {
         if(suffix == "PM" && hour > 12) {
             hour -= 12;
         }
-        return "" + hour + ":" + minute + " " + suffix;
+        // const seconds = d.getSeconds();
+        return "" + (hour.toString().length == 1? ("0" + hour) : hour) + ":" + (minute.toString().length == 1? ("0" + minute) : minute) + " " + suffix;
     }
 
+    // useEffect(() => {
+    //    const currentTime = getTime();
+    //    changeState(currentTime);
+    // });
+
     useEffect(() => {
-       const currentTime = getTime();
-       changeState(currentTime);
-    });
+        const interval = setInterval(() => {
+            const currentTime = getTime();
+            setState(currentTime);
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <div className = "time">
